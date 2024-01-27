97c9f9b38d09e96d00562b9bb6146a67273314da
Martin Aleksandrov <martin_20.05@abv.bg>
2024-01-27 18:24:32 +0200

    Update

--- ASP.NET Advanced/House_Renting.Web/House_Renting.Web/Controllers/AgentController.cs
+++ ASP.NET Advanced/House_Renting.Web/House_Renting.Web/Controllers/AgentController.cs
@@ -3,12 +3,16 @@
     using Microsoft.AspNetCore.Authorization;
     using Microsoft.AspNetCore.Mvc;
 
+    using static Web.Infrastructure.Extensions.ClaimsExtenstions;
+
     [Authorize]
     public class AgentController : Controller
     {
-
+        [HttpGet]
         public async  Task<IActionResult> Become()
         {
+            var agentId = this.User.GetId();
+
              return  View();
         }
     }
---
+++ ASP.NET Advanced/House_Renting.Web/House_Renting.Wrb.Infrastructure/Extensions/ClaimsExtenstions.cs
@@ -0,0 +1,12 @@
+﻿namespace House_Renting.Web.Infrastructure.Extensions
+{
+    using System.Security.Claims;
+
+    public static class ClaimsExtenstions
+    {
+        public static string GetId(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
