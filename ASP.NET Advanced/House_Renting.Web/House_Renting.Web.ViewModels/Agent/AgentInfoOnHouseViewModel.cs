552284ad2dc0bd2a3bb1ad8849c9fd5ee6921dde
Martin Aleksandrov <martin_20.05@abv.bg>
2024-02-28 20:01:48 +0200

    Adding DetailsViewModel and AgentInfoViewModel

---
+++ ASP.NET Advanced/House_Renting.Web/House_Renting.Web.ViewModels/Agent/AgentInfoOnHouseViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace House_Renting.Web.ViewModels.Agent
+{
+    using System.ComponentModel.DataAnnotations;
+    public class AgentInfoOnHouseViewModel
+    {
+
+        public string FullName { get; set; } = null!;
+
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "Phone")]
+        public string PhoneNumber { get; set; } = null!;
+
+    }
+}
---
+++ ASP.NET Advanced/House_Renting.Web/House_Renting.Web.ViewModels/House/HouseDetailsViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace House_Renting.Web.ViewModels.House
+{
+    using House_Renting.Web.ViewModels.Agent;
+
+    public class HouseDetailsViewModel : HouseAllViewModel
+    {
+        public string Description { get; set; } = null!;
+
+        public string Category { get; set; } = null!;
+
+        public AgentInfoOnHouseViewModel Agent { get; set; } = null!;
+    }
+}
